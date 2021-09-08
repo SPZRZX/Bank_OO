c850fb666286f7f7e9fdf186e30bb7c08561b524
SPZRZX <gabriel140419@gmail.com>
2021-09-08 09:41:22 -0300

    Montar a estrutura do Banco

---
+++ myBank/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace myBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaBarbara = new ContaCorrente();
+
+            contaDaBarbara.titular = "Barbara";
+            contaDaBarbara.agencia = 1234;
+            contaDaBarbara.conta = 789;
+            contaDaBarbara.saldo = 100.50;
+
+            ContaCorrente contaDoGabriel = new ContaCorrente();
+
+            contaDoGabriel.titular = "Gabriel";
+            contaDoGabriel.agencia = 1234;
+            contaDoGabriel.conta = 987;
+            contaDoGabriel.saldo = 120;
+
+           Console.WriteLine("O titular da conta é:" + contaDoGabriel.titular);
+
+        }
+    }
+}
