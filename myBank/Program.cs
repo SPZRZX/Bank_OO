12617e315b9fb66bce6c1f3c325b8ec71e2ec1d0
SPZRZX <gabriel140419@gmail.com>
2021-09-15 13:39:58 -0300

    Transferir Depositar e Sacar

--- myBank/Program.cs
+++ myBank/Program.cs
@@ -6,21 +6,25 @@ namespace myBank
     {
         static void Main(string[] args)
         {
-            ContaCorrente contaDaBarbara = new ContaCorrente();
-
-            contaDaBarbara.titular = "Barbara";
+            
+            ContaCorrente contaDaBarbara = new ContaCorrente(Barbara, 1234, 789, 100.50);
+            /*contaDaBarbara.titular = "Barbara";
             contaDaBarbara.agencia = 1234;
             contaDaBarbara.conta = 789;
-            contaDaBarbara.saldo = 100.50;
-
-            ContaCorrente contaDoGabriel = new ContaCorrente();
-
-            contaDoGabriel.titular = "Gabriel";
+            contaDaBarbara.saldo = 100.50; */
+            ContaCorrente contaDoGabriel = new ContaCorrente(Gabriel, 1234, 987, 120);
+            /*contaDoGabriel.titular = "Gabriel";
             contaDoGabriel.agencia = 1234;
             contaDoGabriel.conta = 987;
-            contaDoGabriel.saldo = 120;
+            contaDoGabriel.saldo = 120; */
 
            Console.WriteLine("O titular da conta é:" + contaDoGabriel.titular);
+
+           contaDaBarbara.Sacar(50);
+
+           Console.WriteLine("O saldo da conta é de: " + contaDaBarbara.saldo);
+
+           contaDaBarbara.Depositar(150);
 
         }
     }
